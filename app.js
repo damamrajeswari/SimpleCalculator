30570e5ec317b867654855d300bc68d70a2aa439
Rajeswari Damam <rajeswaridamam007@gmail.com>
2024-09-13 01:45:52 +0530

    HTML,CSS and JS Updated

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 let string="";
 let buttons=document.querySelectorAll('.button');
+let back=document.querySelector('.button-img');
+back.addEventListener('click',(e)=>{
+    string=string.slice(0,-1);
+            document.querySelector('input').value=string;
+})
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
+        if(string[0]=='%'||string[0]=='*'||string[0]=='/'){
+            string="ERROR";
+            document.querySelector('input').value=string;
+        }
         if(e.target.innerHTML=='='){
             string=eval(string);
             document.querySelector('input').value=string;
         }
         else if(e.target.innerHTML=='C'){
-           
-            document.querySelector('input').value="";
+           string="";
+            document.querySelector('input').value=string;
         }
         else{
             console.log(e.target);
